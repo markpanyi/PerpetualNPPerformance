eabb26791a6c4f4a9bdc5c4275095915e0fdef97
奕 潘 <panpanba@outlook.com>
2018-10-22 17:14:47 +0800

    version 2.0.0.1

--- PerpetualNBPerformance/MainForm.cs
+++ PerpetualNBPerformance/MainForm.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Reflection;
 using System.Text;
 using System.Windows.Forms;
 using System.Xml;
